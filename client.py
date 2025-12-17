f0272d540d69b2888b3ffd03be83fdb704a19191
gsaujla <g.s.aujla@outlook.com>
2025-12-17 22:56:24 +0000

    feat: prototype buggy af

---
+++ client.py
@@ -0,0 +1,23 @@
+import requests
+import streamlit as st
+
+st.title("Fastapi chatbot")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message['content'])
+
+if prompt := st.chat_input("Write your prompt in the field"):
+    st.session_state.messages.append({"role": "user", "content":prompt})
+
+response = requests.get(
+    f"http://localhost:8000/generate/text", params={"prompt": prompt}
+)
+
+response.raise_for_status()
+
+with st.chat_message("assistant"):
+    st.markdown(response.text)
